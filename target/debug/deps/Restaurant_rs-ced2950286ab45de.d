06a49a1d25b66a12a7627d7796844a50a250b916
Landon Mote <Landon Mote>
2017-03-09 22:27:59 -0800

    Debug stuff

---
+++ target/debug/deps/Restaurant_rs-ced2950286ab45de.d
@@ -0,0 +1,9 @@
+C:\Users\Landon Mote\Documents\RUST\Restaurant-rs\target\debug\deps\Restaurant_rs-ced2950286ab45de.exe: src\main.rs src\dlist.rs src\party.rs src\table.rs src\restaurant.rs
+
+C:\Users\Landon Mote\Documents\RUST\Restaurant-rs\target\debug\deps\Restaurant_rs-ced2950286ab45de.d: src\main.rs src\dlist.rs src\party.rs src\table.rs src\restaurant.rs
+
+src\main.rs:
+src\dlist.rs:
+src\party.rs:
+src\table.rs:
+src\restaurant.rs:
